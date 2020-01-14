af4593cfa28cfd8838e9afcccd2d670b4dbcc014
nick <nhjlopez@gmail.com>
2020-01-13 19:25:37 -0800

    the className by recurssion is tricky. i think im looping infinitely

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,3 +1,6 @@
+/* eslint-disable func-style */
+/* eslint-disable indent */
+/* eslint-disable space-before-blocks */
 // If life was easy, we could just do things the easy way:
 // var getElementsByClassName = function (className) {
 //   return document.getElementsByClassName(className);
@@ -5,7 +8,25 @@
 
 // But instead we're going to implement it from scratch:
 
+var getElementsByClassName = function(classNAME) {
+	let results = [];
 
-var getElementsByClassName = function(className) {
-  // Your code here
+	function checkNode(node) {
+		if (node.classList) {
+			for (let i = 0; i < node.classList.length; i++) {
+				if (node.classList[i] === classNAME) {
+					results.push(node.classList[i]);
+				}
+			}
+		}
+
+		if (node.childNodes) {
+			for (let x = 0; x < node.childNodes.length; x++) {
+				checkNode(node.childNodes[x]);
+			}
+		}
+	}
+	checkNode(document.body);
+	console.log(results);
+	return results;
 };
